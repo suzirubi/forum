f9513a52adf23df945b34da1c8c1f99ffc69eb1b
Margie Nevarez and Suzi Rubino <margie@>
2017-01-23 13:45:55 -0800

    implement fat arrow forEach to pass through parameters from parent component

--- src/app/post-detail/post-detail.component.ts
+++ src/app/post-detail/post-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-detail',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-detail.component.css']
 })
 export class PostDetailComponent implements OnInit {
-
-  constructor() { }
+  postId: number = null;
+  constructor(private route: ActivatedRoute, private location: Location) {}
 
   ngOnInit() {
+    this.route.params.forEach((urlParametersArray) => {
+      this.postId = parseInt(urlParametersArray['id']);
+    });
   }
 
 }
